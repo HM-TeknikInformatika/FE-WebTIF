36228c44c2452b2b20ebfdbf86fc1225f780d934
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2024-09-26 23:35:15 +0700

    Fix Home Page

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,44 +1,45 @@
 import React, { useState } from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
-import iconImage from "/public/assets/img/Logo.png"
+import iconImage from "/public/assets/img/Logo.png";
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false);
 
+    const handleMenuClick = () => {
+        setMenuOpen(false); // Menutup menu saat item diklik
+    };
 
     return (
-            <div className="navbar-section">
-                <nav className="container">
-            <Link to="/" className="">
-                <a href="">
+        <div className="navbar-section">
+            <nav className="container">
+                <Link to="/" className="">
                     <img src={iconImage} alt="Logo" />
                     <p>HM TIF <br />UNISSULA</p>
-                </a>
-            </Link>
-            <div className="menu" onClick={() => { setMenuOpen(!menuOpen); }}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <ul className={menuOpen ? "open" : ""}>
-                <li>
-                    <NavLink to="/">Beranda</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/tentang">Tentang Kami</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/program">Program Kerja</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/pendaftaran">Pendaftaran</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/kontak">Kontak</NavLink>
-                </li>
-            </ul>
-        </nav>
-            </div>
+                </Link>
+                <div className="menu" onClick={() => { setMenuOpen(!menuOpen); }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className={menuOpen ? "open" : ""}>
+                    <li>
+                        <NavLink to="/" onClick={handleMenuClick}>Beranda</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/tentang" onClick={handleMenuClick}>Tentang Kami</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/program" onClick={handleMenuClick}>Program Kerja</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/pendaftaran" onClick={handleMenuClick}>Pendaftaran</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/kontak" onClick={handleMenuClick}>Kontak</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 };
